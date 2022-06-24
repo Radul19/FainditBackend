685d2134eb3a2f754835decd2cd19e58bd4950bd
Radul19 <radulito19@gmail.com>
2022-06-24 18:28:09 -0400

    week 7

--- src/database.js
+++ src/database.js
@@ -14,24 +14,24 @@ const {Pool} = require("pg")
 
 ///Configuracion para Pool de Postgresql
 
-const config = {
-    user: 'postgres',
-    host: 'localhost',
-    password: "123123",
-    database: 'fainditdb',
-    port:'5432',
-}
-
 // const config = {
-//     user: 'uaklxadebgjtbw',
-//     host: 'ec2-34-198-189-252.compute-1.amazonaws.com',
-//     password: "3ef1d753908f01ad91990def65c06d3a93d91326060645ca1c716bb469d6a3cb",
-//     database: 'ddfq44vngru7o5',
+//     user: 'postgres',
+//     host: 'localhost',
+//     password: "123123",
+//     database: 'fainditdb',
 //     port:'5432',
-//     ssl: {
-//     rejectUnauthorized: false,
-//   },
 // }
+
+const config = {
+    user: 'fcqevrrgrntfim',
+    host: 'ec2-18-204-142-254.compute-1.amazonaws.com',
+    password: "1091dee2e18ee74a8f2fe825206be280d52ef386f6510fea4447137f58978a13",
+    database: 'd8b4f0ctpup4pm',
+    port:'5432',
+    ssl: {
+    rejectUnauthorized: false,
+  },
+}
 
 const pool = new Pool(config)
 
